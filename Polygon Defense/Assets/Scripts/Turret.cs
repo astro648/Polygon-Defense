c90173acf58cd07c69058481bf99d186317a7be6
Ayaan <53174223+astro648@users.noreply.github.com>
2022-12-24 12:21:06 -0800

    turret range visualization

---
+++ Polygon Defense/Assets/Scripts/Turret.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    
+    public Transform target;
+    public float range = 15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
