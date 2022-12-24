fd093882ed6e1ebfa6d12c4c5b302cb329e7f720
Ayaan <53174223+astro648@users.noreply.github.com>
2022-12-24 14:06:31 -0800

    bullet generation

--- Polygon Defense/Assets/Scripts/Turret.cs
+++ Polygon Defense/Assets/Scripts/Turret.cs
@@ -6,15 +6,22 @@ public class Turret : MonoBehaviour
 {
     
     private Transform target;
+
+    [Header("Attributes")]
+
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
     
+    [Header("Unity Setup Fields")]
+
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
-    public float fireRate = 1f;
-    private float fireCountdown = 0f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -64,7 +71,7 @@ public class Turret : MonoBehaviour
     }
 
     void Shoot(){
-        Debug.Log("SHOOT!");
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 
     void OnDrawGizmosSelected(){ // visualizes the range of the turret in editor
