571ff7c50b78f29e9ac1bd47c1a5c43a22a304d3
Ayaan <53174223+astro648@users.noreply.github.com>
2022-12-23 23:15:27 -0800

    5s wave timer

---
+++ Polygon Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    void Update(){
+        if(countdown <= 0f){
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime; // deltaTime is amount of time passed from the last frame drawn
+    }
+
+    void SpawnWave(){
+        Debug.Log("Wave Incoming!");
+    }
+
+}
