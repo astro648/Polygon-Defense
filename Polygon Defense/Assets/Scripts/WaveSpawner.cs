84fa3de4f3901e82fdf8b136b98af0665255371a
Ayaan <53174223+astro648@users.noreply.github.com>
2022-12-23 23:20:56 -0800

    basic enemy spawner; no delay needs fix

--- Polygon Defense/Assets/Scripts/WaveSpawner.cs
+++ Polygon Defense/Assets/Scripts/WaveSpawner.cs
@@ -5,8 +5,12 @@ public class WaveSpawner : MonoBehaviour
     
     public Transform enemyPrefab;
 
+    public Transform spawnPoint;
+
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    private int waveNumber = 1;
 
     void Update(){
         if(countdown <= 0f){
@@ -18,7 +22,16 @@ public class WaveSpawner : MonoBehaviour
     }
 
     void SpawnWave(){
-        Debug.Log("Wave Incoming!");
+
+        for(int i=0; i<waveNumber; i++){
+            SpawnEnemy();
+        }
+
+        waveNumber++;
+    }
+
+    void SpawnEnemy(){
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 
 }
