5f302e5e46c2c9807d2c8987374af4281b9fe284
Ayaan <53174223+astro648@users.noreply.github.com>
2023-02-20 16:04:36 -0800

    camera movement on all axes

--- Polygon Defense/Assets/Scripts/CameraController.cs
+++ Polygon Defense/Assets/Scripts/CameraController.cs
@@ -4,13 +4,26 @@ public class CameraController : MonoBehaviour
 {
 
     public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
 
     void Update()
     {
 
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World); // Time.deltaTime makes speed independent of framerate, Space.World makes it relative to the world coordinates so the movement is independent to the rotation of the camera
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
 
     }
